007bd232a2e1acdbe9526ff980a760c8fb06d685
Rushikesh <fuck@gkk.in>
2024-03-14 16:42:19 +0530

    done by rush

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,59 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import static org.example.TopClass.tc;
+
 abstract class InitAll {
     int x, y, height, width;
     int space_bar = 0;
 }
 
+
+   class TopClass {
+    //global constant variables
+    private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    //global variables
+    //global swing objects
+    private JFrame f = new JFrame("Flappy Bird Redux");
+
+    public JPanel topPanel;
+
+    //other global objects
+    public static TopClass tc = new TopClass();
+
+    /**
+     * Default constructor
+     */
+    public TopClass() {
+    }
+
+    /**
+     * Main executable method invoked when running .jar file
+     * @param args
+     */
+
+
+    /**
+     * Method to construct the JFrame and add the program content
+     */
+     void buildFrame() {
+        Image icon = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("resources/blue_bird.png"));
+        f.setContentPane(createContentPane()); //adds the main content to the frame
+        f.setResizable(true); //true, but game will not function properly unless maximized!
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setAlwaysOnTop(false);
+        f.setVisible(true);
+        f.setMinimumSize(new Dimension(SCREEN_WIDTH*1/4, SCREEN_HEIGHT*1/4)); //set to prevent collapse to tiny window upon resizing
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH); //maximize the JFrame
+        f.setIconImage(icon); //set the icon
+    }
+
+    private JPanel createContentPane() {
+        topPanel = new JPanel(); //top-most JPanel in layout hierarchy
+        return topPanel; //return a blank panel
+    }
+}
 class Bird extends InitAll {
     void jump(){
         // Implement the logic for bird jumping here
@@ -44,7 +92,21 @@ class BirdInputs extends InitAll implements KeyListener {
 
 public class Main {
     public static void main(String[] args) {
-        Bird bird = new Bird(); // Create an instance of Bird
-        BirdInputs birdInputs = new BirdInputs(bird); // Pass the Bird instance to BirdInputs
+
+            //build the GUI on a new thread
+
+            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    tc.buildFrame();
+                    //create a new thread to keep the GUI responsive while the game runs
+                    Thread t = new Thread() {
+                        public void run() {
+                            //in here we will call a function to start the game
+                        }
+                    };
+                    t.start();
+                }
+            });
+
     }
 }
